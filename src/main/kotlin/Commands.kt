afeac1463cc47fbaab491c81c9848912ba0d1cfd
zhou2008 <493505110@qq.com>
2021-05-22 12:37:55 +0800

    NULL

--- src/main/kotlin/Commands.kt
+++ src/main/kotlin/Commands.kt
@@ -5,9 +5,12 @@ import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.MemberCommandSender
 import net.mamoe.mirai.console.command.SimpleCommand
+import net.mamoe.mirai.console.command.UserCommandSender
 import net.mamoe.mirai.event.GlobalEventChannel.subscribeOnce
 import net.mamoe.mirai.event.events.MessageEvent
 import net.mamoe.mirai.message.code.MiraiCode.deserializeMiraiCode
+import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.MessageChainBuilder
 
 class Commands {
     companion object {
@@ -16,6 +19,8 @@ class Commands {
             CommandSay.register()
             CommandTTS.register()
             CommandGNMSMC.register()
+            CommandAtall.register()
+            //CommandQuery.register()
         }
 
         fun unregister() {
@@ -23,6 +28,8 @@ class Commands {
             CommandSay.unregister()
             CommandTTS.unregister()
             CommandGNMSMC.unregister()
+            CommandAtall.unregister()
+            //CommandQuery.unregister()
         }
     }
 }
@@ -42,21 +49,14 @@ object CommandSay : SimpleCommand(
     description = "发送消息"
 ) {
     @Handler
-    suspend fun CommandSender.handle(text: String, count: Int) {
-        val maxCount = Config.MAX_COUNT
-        if (count < maxCount+1) {
-            for (i in 1..count) {
-                sendMessage(text.deserializeMiraiCode())
-            }
-        } else {
-            sendMessage("次数达到上限($maxCount)")
-        }
+    suspend fun CommandSender.handle(text: String) {
+        sendMessage(text.deserializeMiraiCode())
     }
 }
 
 object CommandTTS : SimpleCommand(
     MiraiQQBOT, "tts",
-    description = "文本转语音(只能在群执行)"
+    description = "文本转语音"
 ) {
     @Handler
     suspend fun MemberCommandSender.handle(text: String) {
@@ -69,9 +69,41 @@ object CommandGNMSMC : SimpleCommand(
     description = "获取下一条消息的Mirai码"
 ) {
     @Handler
-    suspend fun CommandSender.handle() {
+    suspend fun UserCommandSender.handle() {
         subscribeOnce<MessageEvent> {
             sendMessage(message.serializeToMiraiCode())
         }
     }
 }
+
+object CommandAtall : SimpleCommand(
+    MiraiQQBOT, "atall",
+    description = "At所有人"
+) {
+    @Handler
+    suspend fun MemberCommandSender.handle() {
+        val messageChainBuilder = MessageChainBuilder()
+        for (member in group.members) {
+            messageChainBuilder.add(At(member))
+        }
+        sendMessage(messageChainBuilder.build())
+    }
+}
+
+//object CommandQuery : SimpleCommand(
+//    MiraiQQBOT, "query",
+//    description = "查询某人在此群的信息"
+//) {
+//    @Handler
+//    suspend fun MemberCommandSender.handle(user: NormalMember) {
+//        val format = SimpleDateFormat("yyyy/MM/dd E HH:mm:ss z")
+//        val lastSpeakTime = format.format(user.lastSpeakTimestamp)
+//        val joinTime = format.format(user.joinTimestamp)
+//        sendMessage(buildMessageChain {
+//            +PlainText("QQ: ${user.id}(${user.nick})\n")
+//            +PlainText("群卡片名称: ${user.nameCard}\n")
+//            +PlainText("最后一次发言时间: $lastSpeakTime\n")
+//            +PlainText("入群时间: $joinTime")
+//        })
+//    }
+//}
--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -6,6 +6,5 @@ import net.mamoe.mirai.console.data.value
 object Config : AutoSavePluginConfig("setting") {
     var APPID: String by value()
     var USERID: String by value("miraiqqbot")
-    var MAX_COUNT: Int by value(10)
     var AUTO_ACCEPT: Boolean by value(false)
 }
